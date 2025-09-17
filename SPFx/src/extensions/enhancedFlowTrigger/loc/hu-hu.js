356a81bbd957709d4d27af2ffcebe84fe711357f
Szőke Péter <peter.szoke92@gmail.com>
2025-09-17 16:07:24 +0200

    Add Hungarian localization for command UI

---
+++ SPFx/src/extensions/enhancedFlowTrigger/loc/hu-hu.js
@@ -0,0 +1,34 @@
+define([], function () {
+  return {
+    TriggerFlowCommandText: "CSV export indítása",
+    SuccessDialogHeader: "Sikeres!",
+    FailedDialogHeader: "Hiba történt...",
+    SuccessDialogSubTextWithMessage:
+      "A folyamat sikeresen elindult, és a következő üzenetet adta vissza: '$message'.",
+    SuccessDialogSubTextWithoutMessage:
+      "A folyamat sikeresen elindult.",
+    FailedDialogSubTextWithMessage:
+      "A folyamat elindult, de a következő üzenettel leállt: '$message'. Próbáld meg újra.",
+    FailedDialogSubTextWithoutMessage:
+      "A folyamat elindult, majd leállt. Próbáld meg újra.",
+    CloseDialogButtonText: "Bezárás",
+    SelectFlowDialogHeader: "Folyamat kiválasztása",
+    SelectFlowDialogSubText:
+      "Kérlek válaszd ki a folyamatot, amit a kiválasztott elemekre szeretnél indítani",
+    SelectFlowDropdownLabel: "Folyamat kiválasztása",
+    SelectFlowDropdownPlaceholder: "Válassz egy lehetőséget",
+    DefaultDialogHeader: "Folyamat indítása",
+    DefaultDialogSubtext:
+      "Kérlek válaszd ki a folyamatot, amit a kiválasztott elemekre szeretnél indítani",
+    InvokedDialogSubText:
+      "A kiválasztott folyamat sikeresen elindult, a háttérben fog lefutni.",
+    CloseDialogUserInstruction: "Ablak bezárható.",
+    WaitingForFlowResponseSpinnerText: "Várakozás a folyamat válaszára...",
+    WaitingForFlowResponseDialogSubText:
+      "A folyamat indítása folyamatban van, ez néhány másodpercet igénybe vehet. Kérlek várj, és ne navigálj el.",
+    WaitingForFlowResponseDialogHeader: "Folyamat indítása",
+    FormInputErrorMessage: "Érvénytelen bevitel",
+    StartFlowButtonText: "Folyamat indítása",
+    InternalServerErrorMessage: "Belső rendszerhiba történt.",
+  };
+});
